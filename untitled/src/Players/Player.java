5b412743c271dc23cf9896efe6705d0f5d6905a7
Aziz <azizrasiodv99@gmail.com>
2025-02-17 14:09:30 +0500

    2

---
+++ untitled/src/Players/Player.java
@@ -0,0 +1,43 @@
+/*import items.Inventory;
+import items.Item;*/
+
+
+
+
+
+package Players;
+
+public class Player {
+    private String playerName;
+    private int playerHealth;
+    private int playerExperience;
+   // private Inventory inventory;
+
+    public Player(String playerName, int playerHealth, int playerExperience
+/*,Inventory inventory*/) {
+        this.playerName = playerName;
+        this.playerHealth = playerHealth;
+        this.playerExperience = playerExperience;
+        //this.inventory = inventory;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getPlayerHealth() {
+        return playerHealth;
+    }
+
+    public void setPlayerHealth(int playerHealth) {
+        this.playerHealth = playerHealth;
+    }
+
+    public int getPlayerExperience() {
+        return playerExperience;
+    }
+
+    public void setPlayerExperience(int playerExperience) {
+        this.playerExperience = playerExperience;
+    }
+}
